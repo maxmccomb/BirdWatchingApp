a3f789997154dafe9cb2364e0adb0e682fa1ab9c
maxwellmccomb16 <maxwellmccomb16@augustana.edu>
2020-02-11 11:45:54 -0600

    Added buttons and fields

--- app/src/main/java/com/example/birdapp/MainActivity.java
+++ app/src/main/java/com/example/birdapp/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.birdapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int WPClickCount = 0;
+    int robinClickCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void WPButtonClick(View v)
+    {
+        System.out.println("WP Clicked");
+        WPClickCount++;
+        System.out.println(WPClickCount);
+        TextView tv = (TextView)findViewById(R.id.textView3);
+        System.out.println("Text View Found");
+        tv.setText(WPClickCount);
+        System.out.println("Changed Text");
+
+    }
+
+    public void robinClickCount(View v){
+        robinClickCount++;
+        TextView tv = (TextView)findViewById(R.id.textView4);
+        tv.setText(robinClickCount);
     }
 }
